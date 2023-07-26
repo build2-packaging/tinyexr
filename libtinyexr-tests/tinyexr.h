6c7f67356c18562a71a5f2e871de9d456c7160ac
lyrahgames <lyrahgames@mailbox.org>
2023-07-26 20:40:27 +0200

    Use testscript to handle hard-coded file paths

---
+++ libtinyexr-tests/tinyexr.h
@@ -0,0 +1,6 @@
+// The tester includes the tinyexr header
+// by refering to the relative path '../../tinyexr.h'.
+// Hence, we use this dummy file at this location
+// to include the library without issues.
+//
+#include <tinyexr.h>
